c7323be8fe66443d5e7ed507d9e9c7c439c66800
Filip <koprivec.filip@gmail.com>
2016-11-27 21:03:00 +0100

    Initial commit

---
+++ naloge/2016/dn1/matrix/FilipKoprivec/fastmatrix.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Filip Koprivec"
+
+from .slowmatrix import SlowMatrix
+
+
+class FastMatrix(SlowMatrix):
+    """
+    Matrika z množenjem s Strassenovim algoritmom.
+    """
+    def multiply(self, left, right):
+        """
+        V trenutno matriko zapiše produkt podanih matrik.
+
+        Množenje izvede s Strassenovim algoritmom.
+        """
+        assert left.ncol() == right.nrow(), \
+               "Dimenzije matrik ne dopuščajo množenja!"
+        assert self.nrow() == left.nrow() and right.ncol() == self.ncol(), \
+               "Dimenzije ciljne matrike ne ustrezajo dimenzijam produkta!"
+        raise NotImplementedError("Naredi sam!")
