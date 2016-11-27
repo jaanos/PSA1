0748b88dd465c98907824a8e79e821bd1a13abbd
LukaAvbreht <l.avbreht@gmail.com>
2016-11-28 00:35:19 +0100

    Impementirana nedelujoca verzija pocasnega mnozenja / ne dela ker neznam pisat zadev v novo matriko oz naredit nove matrike pravilne velikosti, v katero bi zapisal trenutne vrednosti

--- naloge/2016/dn1/matrix/LukaAvbreht/slowmatrix.py
+++ naloge/2016/dn1/matrix/LukaAvbreht/slowmatrix.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from ..matrix import AbstractMatrix
-
+from .. matrix import AbstractMatrix
+#import se ne izvede (SystemError: Parent module '' not loaded, cannot perform relative import)
 class SlowMatrix(AbstractMatrix):
     """
     Matrika z naivnim množenjem.
@@ -16,4 +16,19 @@ class SlowMatrix(AbstractMatrix):
                "Dimenzije matrik ne dopuščajo množenja!"
         assert self.nrow() == left.nrow() and right.ncol() == self.ncol(), \
                "Dimenzije ciljne matrike ne ustrezajo dimenzijam produkta!"
-        raise NotImplementedError("Naredi sam!")
+        res = AbstractMatrix()
+        levaStol = left.ncol()
+        levaVrst = left.nrow()
+        desnaStol = right.ncol()
+        desnaVrst = right.nrow() #ta mora bit enaka k levaStolpci, kar ce ni itak vrnemo v prvi vrstici tko da je nepotrebo
+        res._init_empty(levaVrst,desnaStol) #neznam uporabit prazne matrike
+        for i in range(levaVrst):
+            for j in range(desnaStol):
+                tren = 0
+                for k in range(levaStol):
+                    tren += (left[i,k]*right[k,j])
+                res[i,j] = tren #zracuna uredu vrednost sam ne napise je prav
+                print(tren)
+        return res
+
+
