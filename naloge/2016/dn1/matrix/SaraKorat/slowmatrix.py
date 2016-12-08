f8d3013fca6cd2b0276468522076d01d53a1b50e
Charisa <Sara@Saras-MBP.settopbox.amis.net>
2016-12-08 21:02:50 +0100

    SlowMatrix
    Narejen SlowMatrix s komentarji.

--- naloge/2016/dn1/matrix/SaraKorat/slowmatrix.py
+++ naloge/2016/dn1/matrix/SaraKorat/slowmatrix.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from ..matrix import AbstractMatrix
+#from ..matrix import AbstractMatrix
+
+from matrix import AbstractMatrix
 
 class SlowMatrix(AbstractMatrix):
     """
@@ -16,4 +18,19 @@ class SlowMatrix(AbstractMatrix):
                "Dimenzije matrik ne dopuščajo množenja!"
         assert self.nrow() == left.nrow() and right.ncol() == self.ncol(), \
                "Dimenzije ciljne matrike ne ustrezajo dimenzijam produkta!"
-        raise NotImplementedError("Naredi sam!")
+
+        #raise NotImplementedError("Naredi sam!")
+
+        for i in range(left.nrow()):                # Gremo po vrsticah matrike left.
+            for k in range(right.ncol()):           # Gremo po stolpcih matrike right
+                l = 0                               # Pripravimo število, ki bo po seštevku zmnožkov vrstic matrike left in stolpcev matrike right, enako [i, k] elementu nove matrike.              
+                for j in range(right.nrow()):       # Gremo po vseh elementih prve vrstice matrike left in vseh stolpcih matrike right.
+                    l += left[i,j]*right[j, k]      # Vsak zmnožek prištejemo številu l, da dobimo [i, k]-ti element.
+                self[i, k] = (l)                    # Novi matriki dodamo na novo izračunan element.
+
+        
+
+        
+
+
+        
