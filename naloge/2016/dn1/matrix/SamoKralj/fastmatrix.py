17188fb0dd20e9327a76be48305c985f9cd29480
Samo Kralj <samokralj95@gmail.com>
2016-12-12 20:16:36 +0100

    fastmatrix deluje za vse dimenizje matrik.

--- naloge/2016/dn1/matrix/SamoKralj/fastmatrix.py
+++ naloge/2016/dn1/matrix/SamoKralj/fastmatrix.py
@@ -32,6 +32,7 @@ class FastMatrix(SlowMatrix):
             P5 = (A+D)*(E+H)
             P6 = (B-D)*(G+H)
             P7 = (A-C)*(E+F)
+            """
             print(P1, "\n #######")
             print(P2, "\n #######")
             print(P3, "\n #######")
@@ -39,14 +40,28 @@ class FastMatrix(SlowMatrix):
             print(P5, "\n #######")
             print(P6, "\n #######")
             print(P7, "\n #######")
+            """
             zgornji_levi = P4 + P5 + P6 - P2
             zgornji_desni = P1 + P2
             spodnji_levi = P3 + P4
             spodnji_desni = P1 + P5 - P3 - P7
+            if left.ncol() % 2 == 1:
+                lr2 = left.nrow()//2
+                lc2 = left.ncol()//2
+                lc = left.ncol() - 1
+                rr2 = right.nrow()//2
+                rc2 = right.ncol()//2
+                rr = right.nrow() - 1
+                zgornji_levi += left[:lr2, lc]*right[rr, :rc2]
+                zgornji_desni += left[:lr2, lc] * right[rr, rc2:2*rc2]
+                spodnji_levi += left[lr2:2*lr2, lc] * right[rr, :rc2]
+                spodnji_desni += left[lr2:2*lr2, lc] * right[rr, rc2:2*rc2]
+            """
             print(zgornji_levi, "\n #######%%%%%%")
             print(zgornji_desni, "\n #######%%%%%")
             print(spodnji_levi, "\n #######%%%%%%")
             print(spodnji_desni, "\n #######%%%%%")
+            """
             self[:zgornji_levi.nrow(), :zgornji_levi.ncol()] = zgornji_levi
             self[:zgornji_desni.nrow(), zgornji_levi.ncol():zgornji_levi.ncol() + zgornji_desni.ncol()] = zgornji_desni
             self[zgornji_levi.nrow():zgornji_levi.nrow() + spodnji_levi.nrow(), :spodnji_levi.ncol()] = spodnji_levi
@@ -57,6 +72,7 @@ class FastMatrix(SlowMatrix):
             if right.ncol() % 2 == 1:
                 for k in range(left.nrow()):
                     self[k, right.ncol() - 1] = skalarni_produkt(left[k, :], right[:, right.ncol() - 1])
+            
             
 
 def skalarni_produkt(u, v):
@@ -69,9 +85,10 @@ def razdeli_levo_matriko(M):
     row = M.nrow()//2
     col = M.ncol()//2
     A = M[:row, :col]
-    B = M[:row, col:]
+    B = M[:row, col:2*col]
     C = M[row:2*row, :col]
-    D = M[row:2*row, col:]
+    D = M[row:2*row, col:2*col]
+    """
     print("####")
     print(A)
     print("###")
@@ -81,6 +98,7 @@ def razdeli_levo_matriko(M):
     print("####")
     print(D)
     print("$$$$")
+    """
     return A, B, C, D
 
 def razdeli_desno_matriko(M):
@@ -88,8 +106,9 @@ def razdeli_desno_matriko(M):
     col = M.ncol()//2
     A = M[:row, :col]
     B = M[:row, col:2*col]
-    C = M[row:, :col]
-    D = M[row:, col:2*col]
+    C = M[row:2*row, :col]
+    D = M[row:2*row, col:2*col]
+    """
     print("####")
     print(A)
     print("###")
@@ -99,6 +118,7 @@ def razdeli_desno_matriko(M):
     print("####")
     print(D)
     print("$$$$")
+    """
     return A, B, C, D
 
     
