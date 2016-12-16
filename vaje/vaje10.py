7e924c165bd752046de9d8e15cab758a7060275e
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-12-16 20:06:45 +0100

    Dodani komentarji in preverjanje monotonosti

--- vaje/vaje10.py
+++ vaje/vaje10.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def strnjenoPodzaporedje(a):
+    """
+    Strnjeno podzaporedje z največjo vsoto.
+
+    Časovna zahtevnost: O(n)
+    """
     n = len(a)
     s = [(0, 0)]
     for i in range(n):
@@ -9,8 +14,20 @@ def strnjenoPodzaporedje(a):
     return max((v, z, k) for k, (v, z) in enumerate(s))
 
 def optimalnaPot(a, m):
+    """
+    Poišče točke, kjer se ustavimo,
+    da minimiziramo kazen zaradi neoptimalne razdalje med njimi.
+    Seznam a mora strogo naraščati z začetnim členom 0.
+
+    Kazen za posamezen interval je definirana kot (m-x)^2,
+    kjer je x dolžina intervala.
+    Funkcija vrne skupno kazen in indekse, kjer se ustavimo.
+
+    Časovna zahtevnost: O(n^2)
+    """
     assert a[0] == 0
     n = len(a)
+    assert all(a[i] > a[i-1] for i in range(1, n))
     s = [(0, None)]
     for i in range(n-1):
         s.append(min((s[j][0] + (m-a[i+1]+a[j])**2, j) for j in range(i+1)))
@@ -22,6 +39,11 @@ def optimalnaPot(a, m):
     return (s[-1][0], list(reversed(pot)))
 
 def besede(s, beseda):
+    """
+    Razbije niz s na veljavne besede, na katerih funkcija beseda vrne True.
+
+    Časovna zahtevnost: O(n^2) klicev funkcije beseda
+    """
     n = len(s)
     d = [0]
     for i in range(n):
