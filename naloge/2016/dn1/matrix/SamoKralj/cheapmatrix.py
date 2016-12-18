561105e1b93f37be103ff24c8b827ab63d956f20
Samo Kralj <samokralj95@gmail.com>
2016-12-18 19:37:02 +0100

    Uspešno implementiran CheapMatrix.

--- naloge/2016/dn1/matrix/SamoKralj/cheapmatrix.py
+++ naloge/2016/dn1/matrix/SamoKralj/cheapmatrix.py
@@ -25,10 +25,10 @@ class CheapMatrix(SlowMatrix):
             
         if left.nrow() == 1:
             for k in range(right.ncol()):
-                self[0, k] = skalarni_produkt(left[0,:], right[:, k])
+                self[0,k] = skalarni_produkt(left[0,:], right[:, k])
         elif right.ncol() == 1:
             for k in range(left.nrow()):
-                self[k, 0] = skalarni_produkt(left[k,:], right[0, k])
+                self[k, 0] = skalarni_produkt(left[k,:], right[:, 0])
         else:
             """
             Imamo matrike velikosti (2*n + 1) x (2*m + 1): 
@@ -66,10 +66,10 @@ class CheapMatrix(SlowMatrix):
             S3 = self[visina_l:2*visina_l, :sirina_r]
             S4 = self[visina_l:2*visina_l, sirina_r:2*sirina_r]
 
-            print(A,B,C,D,E,F,G,H)
-            print("#####")
-            print(WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
+            #print(A,B,C,D,E,F,G,H)
+            #print("#####")
+            #print(WORK_1, WORK_2, WORK_3, WORK_4)
+            #print(self)
 
             WORK_1.multiply(A,F,WORK_4) #A*F
             WORK_2.multiply(A,H,WORK_3) #A*H
@@ -78,102 +78,159 @@ class CheapMatrix(SlowMatrix):
             WORK_3.multiply(B, H, WORK_4) #B*H
             WORK_3 += WORK_2 #P2
 
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
+            #print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
+            #print(self)
 
             S2[:,:] = WORK_1 #P1
-            S2 += WORK_3 #P1 + P2  --P2 sedaj lahko pozabimo
+            S2 += WORK_3 #P1 + P2
 
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            clear(WORK_3)
-            WORK_3.multiply(A, E, S1) #A*E
-            S3.multiply(D, E, S1) #D*E
-            WORK_4.multiply(D, H, S1) #D*H
-
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            S4[:,:] = WORK_1 #To je zadnjic ko bomo uporabili P1
-            clear(WORK_1)
-            WORK_1[:,:] = WORK_3
-            WORK_1 += WORK_2
-            WORK_1 += S3
-            WORK_1 += WORK_4 #Na mestu WORK_1 je sedaj shranjena matrika P5
-
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)            
-            clear(WORK_2)
-            WORK_2.multiply(C, E, S1) # C*E
-            S3 += WORK_2 # C*E + D*E = P3, Na polju S3 imamo sedaj P3 matriko
-
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            WORK_3 -= WORK_2 #WORK_3 = A*E - C*E
-            clear(WORK_2)
-            WORK_2.multiply(A, F, S1)
-            WORK_3 += WORK_2 #WORK_3 = A*E - C*E + A*F
-
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            clear(WORK_2)
-            WORK_2.multiply(C,F, S1)
-            WORK_3 -= WORK_2 # WORK_3 = A*E - C*E + A*F - C*F = P7
-
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            S4 += WORK_1
-            S4 -= S3
-            S4 -= WORK_3
-
-            print("####", S4)
-
-            clear(WORK_2)
-            WORK_2.multiply(D, G, S1) # D*G
+            #print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
+            #print(self)
             clear(WORK_4)
-            WORK_4.multiply(D, E, S1) # D*E
-            WORK_2 -= WORK_4 # D*G - D*E = P4
+            WORK_4.multiply(A, E, S1) #A*E
+            S3.multiply(D, E, S1) #D*E
+            S4.multiply(D, H, S1) #D*H
 
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            S3 += WORK_2 #Sedaj moramo urediti le še polje S1 = P4 + P5 + P6 + P7
+            WORK_2 += WORK_4
+            WORK_2 += S3
+            WORK_2 += S4 #WORK_2 = A*E + A*H + D*E + D*H = P5
+            clear(S4)
+            S4[:,:] = WORK_1 #To je zadnjic ko bomo uporabili P1
+
+            #print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
+            #print(self)            
+
+            clear(WORK_4)
+            WORK_4.multiply(C, E, S1) # C*E
+            S3 += WORK_4 #S3 = D*E + C*E = P3
+
+            S4 += WORK_2
+            S4 -= S3
+            S4 += WORK_4 # S4 = P1 + P5 - P3 + C*E (manjka še - A*E - A*F + C*F)
+
+            clear(WORK_4)
+            WORK_4.multiply(A, E, S1)
+            S4 -= WORK_4
+
+            clear(WORK_4)
+            WORK_4.multiply(A,F, S1)
+            S4 -= WORK_4
+
+            clear(WORK_4)
+            WORK_4.multiply(C, F, S1)
+            S4 += WORK_4 #S4 = P1 + P5 - P3 - P7
+
+            """
+            Trenutno stanje:
+            S1 = ??
+            S2 = Pravilna vrednost
+            S3 = P3
+            S4 = Pravilna vrednost
+            WORK_1 = P1
+            WORK_2 = P5
+            WORK_3 = P2
+            WORK_4 = C*F
+            """
+
+            clear(WORK_1)
+            WORK_1.multiply(D, G, S1)
+            S3 += WORK_1
+
+            clear(WORK_4)
+            WORK_4.multiply(D, E, S1)
+            S3 -= WORK_4
+
+            """
+            Trenutno stanje:
+            S1 = ??
+            S2 = Pravilna vrednost
+            S3 = Pravilna Vrednost
+            S4 = Pravilna vrednost
+            WORK_1 = D*G
+            WORK_2 = P5
+            WORK_3 = P2
+            WORK_4 = D*E
+            """
 
             clear(S1)
+            S1[:,:] = WORK_1
+            S1 -= WORK_4 #S1 = P4
+            S1 += WORK_2 #S1 = P4 + P5
+            S1 -= WORK_3 #S1 = P4 + P5 - P2
+            S1 -= WORK_1 #S1 = P4 + P5 - P2 - D*G
+
+            clear(WORK_2)
+            WORK_2.multiply(B,G, WORK_4)
             S1 += WORK_2
-            S1 += WORK_3
-            S1 += WORK_1 # S1 = P4 + P5 + P7
 
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)
-            #Na mestu S1 moramo sedaj le še prišteti P6 = B*G + B*H - D*G - D*H
-            clear(WORK_1)
-            WORK_1.multiply(B, G, WORK_4)
-            S1 += WORK_1
+            clear(WORK_2)
+            WORK_2.multiply(B,H, WORK_4)
+            S1 += WORK_2
 
-            clear(WORK_1)
-            WORK_1.multiply(B, H, WORK_4)
-            S1 += WORK_1
+            clear(WORK_2)
+            WORK_2.multiply(D, H, WORK_4)
+            S1 -= WORK_2
+
+            if left.ncol() % 2 == 1:
+                """
+                V primeru, da ima leva matrika liho število stolpcev moramo
+                vsaki podmatriki dodati še nek člen, kar je vidno iz izpeljave.
+
+                X = | A B x | 
+                    | C D y |
+                    
+                Y = | E F |
+                    | G H |
+                    | z w |
+
+                Kjer so x,y in z,w podmatrike, ki imajo širino oz. višino 1.
+
+                V produktu X*Y bo v zgornjem levem kotu člen:
+
+                A*E + B*G + x * z
+
+                A*E + B*G dobimo z strassenovim algoritmom za sodo število stolpcev.
+                Člen x*z pa moramo dodati sami.
+
+                Podoben razmislek tudi za ostale 3 podmatrike.
+                """
+                RR = right.nrow() - 1
+                LC = left.ncol() - 1
+
+                clear(WORK_1)
+                WORK_1.multiply(left[:visina_l,LC], right[RR, :sirina_r])
+                S1 += WORK_1
+
+                clear(WORK_1)
+                WORK_1.multiply(left[:visina_l, LC], right[RR, sirina_r:2*sirina_r])
+                S2 += WORK_1
+
+                clear(WORK_1)
+                WORK_1.multiply(left[visina_l:2*visina_l, LC], right[RR, :sirina_r])
+                S3 += WORK_1
+
+                clear(WORK_1)
+                WORK_1.multiply(left[visina_l:2*visina_l, LC], right[RR, sirina_r:2*sirina_r])
+                S4 += WORK_1
             
-            clear(WORK_1)
-            WORK_1.multiply(D,G, WORK_4)
-            S1 -= WORK_1
+            if left.nrow() % 2 == 1:
+                """
+                Ročno primnožimo še najnižjo vrstico, ki je smo jo pri
+                delitvi na podmatrike izpustili.
+                """
+                LR = left.nrow() - 1
+                for k in range(right.ncol()):
+                    self[LR, k] = skalarni_produkt(left[LR, :], right[:, k])
 
-            clear(WORK_1)
-            WORK_1.multiply(D, H, WORK_4)
-            S1 -= WORK_1 
+            if right.ncol() % 2 == 1:
+                """
+                Primnožimo še zadnji stolpec.
+                """
+                RC = right.ncol() - 1
+                for k in range(left.nrow()):
+                    self[k, RC] = skalarni_produkt(left[k, :], right[:, RC])
 
-            print(S1, S2, S3, S4, WORK_1, WORK_2, WORK_3, WORK_4)
-            print(self)            
             
-            
-
-            
-            
-
-            
-            
-
-
-
 
 def skalarni_produkt(u, v):
     """
