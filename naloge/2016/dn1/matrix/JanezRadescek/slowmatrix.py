0d3525b7d0b2fc309b6ed8b9cf5354e383ff6870
JanezRadescek <janez.radescek@gmail.com>
2016-12-19 21:09:39 +0100

    SlowMatrix

--- naloge/2016/dn1/matrix/JanezRadescek/slowmatrix.py
+++ naloge/2016/dn1/matrix/JanezRadescek/slowmatrix.py
@@ -16,4 +16,17 @@ class SlowMatrix(AbstractMatrix):
                "Dimenzije matrik ne dopuščajo množenja!"
         assert self.nrow() == left.nrow() and right.ncol() == self.ncol(), \
                "Dimenzije ciljne matrike ne ustrezajo dimenzijam produkta!"
+
+
+
+        for i in range(self.nrow()):
+            for j in range(self.ncol()):
+                ij = 0;
+                for k in range(left.ncol):
+                    ij += left[i,k] * right[j,k];
+                self[i,j] = ij;
+
+
+
+
         raise NotImplementedError("Naredi sam!")
