31db9b3ac5c958dde71b17a75697d18c77187766
EvaZmazek <eva.zmazek@student.fmf.uni-lj.si>
2016-12-20 17:27:23 +0100

    Zaznana napaka (prej sem ustvarjala nove matrike, zdaj poskušam porabljati čimmanj prostora)

--- naloge/2016/dn1/matrix/EvaZmazek/cheapmatrix.py
+++ naloge/2016/dn1/matrix/EvaZmazek/cheapmatrix.py
@@ -70,67 +70,67 @@ class CheapMatrix(SlowMatrix):
 
         if stStolpcevLeveMatrike % 2 == 0 and stVrsticLeveMatrike % 2 == 0 and stStolpcevDesneMatrike % 2 == 0:
 
-            work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] = \
-                left[0:stVrsticLeveMatrike // 2, 0:stStolpcevLeveMatrike // 2] * \
-                (right[0:stStolpcevLeveMatrike // 2, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike] - \
-                 right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike])
-            #v zgornjo levo cetrtino delovne matrike vpisemo P1 = \
-            #A = ... * \
-            #(F = ... - \
-            # H = ...)
+            #najprej si naredimo pointerje, ki ne vzamejo nič dodatnega prostora
+
+            #cetrine leve matrike
+            A = left[0:stVrsticLeveMatrike // 2, 0:stStolpcevLeveMatrike // 2]
+            B = left[0:stVrsticLeveMatrike // 2, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike]
+            C = left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, 0:stStolpcevLeveMatrike // 2]
+            D = left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike]
+
+            #cetrine desne matrike
+            E = right[0:stStolpcevLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2]
+            F = right[0:stStolpcevLeveMatrike // 2, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike]
+            G = right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, 0:stStolpcevDesneMatrike // 2]
+            H = right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike]
+
+            #cetrtine delovne matrike
+            D1 = work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2]
+            D2 = work[0:stVrsticLeveMatrike // 2, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike]
+            D3 = work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2]
+            D4 = work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike,stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike]
+
+            F -= H
+            D1.multiply(A,F)
+            #v zgornjo levo cetrtino delovne matrike vpisemo P1 = A  * (F - H)
+            F += H
 
             #v tem koraku naredimo eno rekurzivno množenje ter eno odštevanje (prištevanje nasprotnih vrednosti), za kar porabimo
             #T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(k//2 * m//2) dodatnega časa za odštevanje
             #P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za odštevanje
 
-            work[0:stVrsticLeveMatrike // 2, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike] = \
-                (left[0:stVrsticLeveMatrike // 2, 0:stStolpcevLeveMatrike // 2] + \
-                 left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike]) * \
-                (right[0:stStolpcevLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] + \
-                 right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike])
-            #v zgorno desno cetrtino delovne matrike vpisemo P5 = \
-            #(A = ... + \
-            #D = ... ) * \
-            #(E = ... + \
-            #H = ... )
+            A += D
+            E += H
+            D2.multiply(A,E)
+            #v zgorno desno cetrtino delovne matrike vpisemo P5 = (A + D) * (E + H)
+            A -= D
+            E -= H
 
             # v tem koraku naredimo eno rekurzivno množenje ter dve seštevanji, za kar porabimo
             # T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(n//2 * k//2) + O(k//2 * m//2) = O((n+m)//2 * k//2) dodatnega časa za seštevanje
             # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanje
 
-            work[stVrsticLeveMatrike // 2 : stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] = \
-                (left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, 0:stStolpcevLeveMatrike // 2] + \
-                 left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike]) * \
-                right[0:stStolpcevLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2]
-            #v spodnjo levo cetrtino delovne matrike vpisemo P3 = \
-            #(C = ... + \
-            #D = ... ) * \
-            #E = ...
+            C += D
+            D3.multiply(C,E)
+            #v spodnjo levo cetrtino delovne matrike vpisemo P3 = (C + D) * E
+            C -= D
 
             # v tem koraku naredimo eno rekurzivno množenje ter eno seštevanje, za kar porabimo
             # T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(n//2 * k//2) dodatnega časa za seštevanje
             # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanje
 
-            work[stVrsticLeveMatrike // 2 : stVrsticLeveMatrike, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike] = \
-                (left[0:stVrsticLeveMatrike // 2, 0:stStolpcevLeveMatrike // 2] - \
-                 left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, 0:stStolpcevLeveMatrike // 2]) * \
-                (right[0:stStolpcevLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] + \
-                 right[0:stStolpcevLeveMatrike // 2, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike])
-            #v spodnjo desno cetrtino delovne matrike vpisemo P7 = \
-            #(A = ... - \
-            #C = ... ) * \
-            #(E = ... + \
-            #F = ... )
+            A -= C
+            E += F
+            D4.multiply(A,E)
+            #v spodnjo desno cetrtino delovne matrike vpisemo P7 = (A - C ) * (E + F)
+            A += C
+            E -= F
 
             # v tem koraku naredimo eno rekurzivno množenje ter eno seštevanje in eno odštevanje, za kar porabimo
             # T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(n//2 * k//2) + O(k//2 * m//2) = O((n+m)//2 * k//2) dodatnega časa za seštevanje in odštevanje
             # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanje in odštevanje
 
-            self[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike] = \
-                work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] + \
-                work[0:stVrsticLeveMatrike // 2, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike] - \
-                work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] - \
-                work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike,stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike]
+            self[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike] = D1 + D2 - D3 - D4
             #spodnjo desno cetrtino koncne matrike nastavimo na
             #P1 = ... + \
             #P5 = ... - \
@@ -143,25 +143,17 @@ class CheapMatrix(SlowMatrix):
 
             #na tem koraku ne potrebujemo več P7, torej na njegovo mesto napišemo P2 (shranjene bomo imeli P1, P5, P3 in P2)
 
-            work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike] = \
-                (left[0:stVrsticLeveMatrike // 2, 0:stStolpcevLeveMatrike // 2] + \
-                 left[0:stVrsticLeveMatrike // 2, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike]) * \
-                right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike]
-            #v spodnjo desno cetrtino delovne matrike si torej zapisemo P2 = \
-            #(A = ... + \
-            #B = ... ) * \
-            #H = ...
+            A += B
+            D4.multiply(A,H)
+            #v spodnjo desno cetrtino delovne matrike si torej zapisemo P2 = (A + B ) * H
+            A -= B
 
             # v tem koraku naredimo eno rekurzivno množenje ter eno seštevanje, za kar porabimo
             # T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(n//2 * k//2) dodatnega časa za seštevanje
             # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanje
 
-            self[0:stVrsticLeveMatrike // 2, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike] = \
-                work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] + \
-                work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike]
-            #zgornjo desno cetrtino matrike nastavimo na
-            #P1 = ... + \
-            #P2 = ...
+            self[0:stVrsticLeveMatrike // 2, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike] = D1 + D4
+            #zgornjo desno cetrtino matrike nastavimo na P1 + P2
 
             # v tem koraku naredimo 1 seštevanj2, za katero porabimo
             # O(n//2 * m//2) dodatnega časa
@@ -169,25 +161,17 @@ class CheapMatrix(SlowMatrix):
 
             #na tem koraku ne potrebujemo več P1, zato ga nadomestimo s P4 (shranjene bomo imeli P4, P5, P3 in P2)
 
-            work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] = \
-                left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike] * \
-                (right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, 0:stStolpcevDesneMatrike // 2] - \
-                 right[0:stStolpcevLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2])
-            #zgornjo levo cetrtino delovne matrike nastavimo na P4 = \
-            #D = ... * \
-            #(G = ... - \
-            #E = ... )
+            G -= E
+            D1.multiply(D,G)
+            #zgornjo levo cetrtino delovne matrike nastavimo na P4 = D * (G - E)
+            G +=E
 
             # v tem koraku naredimo eno rekurzivno množenje ter eno odštevanje, za kar porabimo
             # T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(k//2 * m//2) dodatnega časa za odštevanje
-            # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanje
+            # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanj
 
-            self[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] = \
-                work[stVrsticLeveMatrike // 2 : stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] + \
-                work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2]
-            #spodnjo levo cetrtino koncne matrike nastavimo na
-            #P3 = ... + \
-            #P4 = ...
+            self[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] = D3 + D1
+            #spodnjo levo cetrtino koncne matrike nastavimo na P3 + P4
 
             # v tem koraku naredimo eno seštevanje, za katero porabimo
             # O(n//2 * m//2) dodatnega časa
@@ -196,31 +180,19 @@ class CheapMatrix(SlowMatrix):
 
             #na tem koraku ne potrebujemo več P3, zato ga nadomestimo s P6 (shranjene bomo imeli P4, P5, P6 in P2)
 
-            work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] = \
-                (left[0:stVrsticLeveMatrike // 2, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike] - \
-                 left[stVrsticLeveMatrike // 2:stVrsticLeveMatrike, stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike]) * \
-                (right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, 0:stStolpcevDesneMatrike // 2] + \
-                 right[stStolpcevLeveMatrike // 2:stStolpcevLeveMatrike, stStolpcevDesneMatrike // 2:stStolpcevDesneMatrike])
-            #spodnjo levo cetrtino delovne matrike nastavimo na P6 = \
-            #(B = ... - \
-            #D = ... ) * \
-            #(G = ... + \
-            #H = ... )
+            B -= D
+            G += H
+            D3.multiply(B,G)
+            #spodnjo levo cetrtino delovne matrike nastavimo na P6 = (B - D) * (G + H)
+            B += D
+            G -= H
 
             # v tem koraku naredimo eno rekurzivno množenje ter eno seštevanje in eno odštevanje, za kar porabimo
             # T(n//2,k//2,m//2) dodatnega časa za rekurzivno množenje in O(n//2 * k//2) + O(k//2 * m//2) = O((m+n)//2 * k//2) dodatnega časa za seštevanje in odštevanje
             # P(n//2,k//2,m//2) dodanega prostora za rekurzivno množenje in nič dodatnega prostora za seštevanje in odštevanje
 
-            self[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] = \
-                work[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] + \
-                work[0:stVrsticLeveMatrike // 2, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike] + \
-                work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, 0:stStolpcevDesneMatrike // 2] - \
-                work[stVrsticLeveMatrike // 2: stVrsticLeveMatrike, stStolpcevDesneMatrike // 2: stStolpcevDesneMatrike]
-            #zgornjo desno cetrtino koncne matrike nastavimo na
-            #P4 = ... + \
-            #P5 = ... + \
-            #P6 = ... - \
-            #P2 = ...
+            self[0:stVrsticLeveMatrike // 2, 0:stStolpcevDesneMatrike // 2] = D1 + D2 + D3 - D4
+            #zgornjo desno cetrtino koncne matrike nastavimo na P4 + P5 + P6 - P2
 
             # v tem koraku naredimo 3 seštevanja, za kar porabimo
             # 3*O(n//2 * m//2) = O(n//2 * m//2) dodatnega časa
