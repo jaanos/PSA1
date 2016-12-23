d7d7b024ba91b28fee2b6db9442516950b3ae287
JanezRadescek <janez.radescek@gmail.com>
2016-12-23 18:09:36 +0100

    Cheap_začetek

--- naloge/2016/dn1/matrix/JanezRadescek/cheapmatrix.py
+++ naloge/2016/dn1/matrix/JanezRadescek/cheapmatrix.py
@@ -28,62 +28,74 @@ class CheapMatrix(SlowMatrix):
         aaa = self.nrow()
         bbb = self.ncol()
         ujemanje = left.ncol()
+        work *= 0
 
         #če imamo množenje z vektorjem zmnožimo na običajen način
         if (aaa == 1) or (bbb == 1) or (ujemanje == 1):
             super(CheapMatrix, self).multiply(left,right)
 
-        #poskrbimo če stranice matrik niso sode
 
+        #poskrbimo če stranice matrik niso sode
         elif aaa%2 == 1:
             #self[:aaa-1,:] = self[:aaa-1,:].multiply(left[:aaa-1,:],right)
             #self[aaa-1,:] = self[aaa-1,:].multiply(left[aaa-1,:],right)
-
-            self[:aaa-1,:] = left[:aaa-1,:] * right
-            self[aaa-1,:] = left[aaa-1,:] * right
+            self[:aaa-1,:].multiply(left[:aaa-1,:],  right)
+            self[aaa-1,:].multiply(left[aaa-1,:],  right)
 
         elif bbb%2 == 1:
             #self[:,:bbb-1] = self[:,:bbb-1].multiply(left, right[:,:bbb-1])
             #self[:,bbb-1] = self[:,bbb-1].multiply(left, right[:,bbb-1])
 
-            self[:,:bbb-1] = left * right[:,:bbb-1]
-            self[:,bbb-1] = left * right[:,bbb-1]
+            self[:,:bbb-1].multiply(left, right[:,:bbb-1])
+            self[:,bbb-1].multiply(left, right[:,bbb-1])
 
 
         elif ujemanje%2 == 1:
             #self[:,:] = self.multiply(left[:,:ujemanje-1], right[:ujemanje-1,:]) + self.multiply(left[:,ujemanje-1], right[ujemanje-1,:])
-            self[:,:] = left[:,:ujemanje-1] * right[:ujemanje-1,:] + left[:,ujemanje-1] * right[ujemanje-1,:]
+            #self[:,:] = left[:,:ujemanje-1] * right[:ujemanje-1,:] + left[:,ujemanje-1] * right[ujemanje-1,:]
+            self.multiply(left[:,:ujemanje-1], right[:ujemanje-1,:])
+            self += work.multiply(left[:,ujemanje-1], right[ujemanje-1,:])
+
 
         #če so sode
-
-
         else:
-            a = aaa / 2
-            b = bbb / 2
-            c = ujemanje / 2
+            a = aaa // 2
+            b = bbb // 2
+            c = ujemanje // 2
 
             #podmatrike
-            A = left[:a,:c]
-            B = left[:a,c:]
-            C = left[a:,:c]
-            D = left[a:,c:]
+            L11 = left[:a,:c]
+            L12 = left[:a,c:]
+            L21 = left[a:,:c]
+            L22 = left[a:,c:]
 
-            E = right[:c,:b]
-            F = right[:c,b:]
-            G = right[c:,:b]
-            H = right[c:,b:]
+            R11 = right[:c,:b]
+            R12 = right[:c,b:]
+            R21 = right[c:,:b]
+            R22 = right[c:,b:]
 
-            #7 množenj
-            p1 = A * (F - H)
-            p2 = (A + B) * H
-            p3 = (C + D) *E
-            p4 = D * (G - E)
-            p5 = (A + D) * (E + H)
-            p6 = (B - D) * (G + H)
-            p7 = (A - C) * (E + F)
+            S11 = self[:a,:b]
+            S12 = self[:a,b:]
+            S21 = self[a:,:b]
+            S22 = self[a:,b:]
 
-            #seštevanje
-            self[:a,:b] = p4 + p5 + p6 -p2
-            self[:a,b:] = p1 + p2
-            self[a:,:b] = p3 + p4
-            self[a:,b:] = p1 + p5 - p3 -p7
+            W11 = work[:a,:b]
+            W12 = work[:a,b:]
+            W21 = work[a:,:b]
+            W22 = work[a:,b:]
+
+            #P1
+            R12-=R22
+            W12.multiply(L11, R12)
+            R12+=R22
+            S12+=W12
+            S22+=W12
+
+            #P2
+            L11+=L12
+            W12.multiply(L11, R22)
+            L11-=L12
+            S11-=W12
+            S12+=W12
+
+            #p3 
