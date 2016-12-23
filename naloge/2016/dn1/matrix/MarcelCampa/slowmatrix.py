c76d09b38a3daab7172f0072d3b8340193d5b7cd
campovski <campovski@gmail.com>
2016-12-23 22:00:31 +0100

    Add pre-final version of the project, only graph missing

---
+++ naloge/2016/dn1/matrix/MarcelCampa/slowmatrix.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from ..matrix import AbstractMatrix
+
+class SlowMatrix(AbstractMatrix):
+    """
+    Matrika z naivnim množenjem.
+    """
+    def multiply(self, left, right):
+        """
+        V trenutno matriko zapiše produkt podanih matrik.
+
+        Množenje izvede z izračunom skalarnih produktov
+        vrstic prve in stolpcev druge matrike.
+        """
+        assert left.ncol() == right.nrow(), \
+               "Dimenzije matrik ne dopuščajo množenja!"
+        assert self.nrow() == left.nrow() and right.ncol() == self.ncol(), \
+               "Dimenzije ciljne matrike ne ustrezajo dimenzijam produkta!"
+        
+        self *= 0
+        for i in range(0, left.nrow()):
+            for j in range(0, right.ncol()):
+                for k in range(0, left.ncol()):
+                    self[i, j] += left[i, k] * right[k, j]
+                    
+        return self
