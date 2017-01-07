2ec5b73c5c2119914bf06207555900a7a13283e1
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2017-01-07 01:16:25 +0100

    Dodan algoritem za določanje optimalnega vrstnega reda množenja matrik

--- vaje/vaje12.py
+++ vaje/vaje12.py
@@ -32,3 +32,17 @@ def longestCommonSubsequence(x, y):
                 V[i+1, j+1] = max(V[i, j+1], V[i+1, j])
     return V[n, m]
 
+def matrike(m, st = lambda a, b, c: a*b*c):
+    n = len(m) - 1
+    v = {(i, i+1): (0, None) for i in range(n)}
+    for i in range(2, n+1):
+        for j in range(n-i+1):
+            v[j, j+i] = min((v[j, k][0] + v[k, j+i][0] +
+                             st(m[j], m[k], m[j+i]), k)
+                            for k in range(j+1, j+i))
+    def oklepaji(i, j):
+        if i == j-1:
+            return i
+        k = v[i, j][1]
+        return [oklepaji(i, k), oklepaji(k, j)]
+    return (v[0, n][0], oklepaji(0, n))
