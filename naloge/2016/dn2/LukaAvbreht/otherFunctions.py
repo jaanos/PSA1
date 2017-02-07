b38f461165dcfc73e6c7247414c6b42f82f8bd8e
LukaAvbreht <l.avbreht@gmail.com>
2017-02-07 22:14:20 +0100

    Fixed the function to list all the valid children

--- naloge/2016/dn2/LukaAvbreht/otherFunctions.py
+++ naloge/2016/dn2/LukaAvbreht/otherFunctions.py
@@ -35,7 +35,11 @@ def list_primernih_otrok(parent):
                     tren.append(res[j]+'0')
             k = k*2-dif
         res = tren
-    return res
+    tren = list()
+    for i in res:
+        if valid_cycle(i):
+            tren.append(i)
+    return tren
 
 def valid_cycle(cycle):
     """Returns True if a cycle is valid and False othervise (Cycle is valid ifi it has an independent subgroup of ones)"""
@@ -44,8 +48,6 @@ def valid_cycle(cycle):
             if cycle[i-1] != '0' or cycle[i+1] != '0':
                 return False
     return True
-
-
 
 def are_neighbours(a,b,T):  # aLso returns false if you compere by
         """Returns True if two nodes are neighbours, otherwise returns False"""
@@ -59,10 +61,27 @@ def are_neighbours(a,b,T):  # aLso returns false if you compere by
             return True
         return False
 
-print(valid_cycle('11000101'))
-print(valid_cycle('101000101'))
-print(valid_cycle('0101000101'))
-tre = list_primernih_otrok('0101000101')
-print(tre)
-for i in tre:
+def all_valid_cycles(n):
+    """Returns the list of all valid cycles of length n"""
+    res = ['1','0']
+    k = 2
+    for i in range(n-1):
+        tren = list()
+        dif = 0
+        for j in range(k):
+            if res[j][-1]=='1':
+                tren.append(res[j]+'0')
+                dif += 1
+            else:
+                tren.append(res[j]+'1')
+                tren.append(res[j]+'0')
+            k = k*2-dif
+        res = tren
+    return res
+
+#print(all_valid_cycles(4))
+
+print(list_primernih_otrok('0100'))
+
+for i in list_primernih_otrok('0100'):
     print(valid_cycle(i))
