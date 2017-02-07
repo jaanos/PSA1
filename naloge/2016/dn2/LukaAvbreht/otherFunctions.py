e0ff9ec10daf0ad4491c1878fcd898286d6662ae
LukaAvbreht <l.avbreht@gmail.com>
2017-02-07 17:55:21 +0100

    Added the skeleton of functions to use in the algorithm.

--- naloge/2016/dn2/LukaAvbreht/otherFunctions.py
+++ naloge/2016/dn2/LukaAvbreht/otherFunctions.py
@@ -1,3 +1,10 @@
 
 
+def primeren_otrok(parent,child):
+    """It returns true if the ciycle child can ba a neighbour cycle of parent (input is in binary)"""
 
+
+    return True
+
+def list_primernih_otrok(pranet):
+    """Returns the list of all valid children of a parent as a set of binary numbers"""
