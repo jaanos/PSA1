5be052ee98abc115bd881914563f67e8fe77cde0
EvaZmazek <eva.zmazek@student.fmf.uni-lj.si>
2017-02-07 21:31:31 +0100

    izbrisani testni primeri iz glavne datoteke

--- naloge/2016/dn2/EvaZmazek/maxindepset.py
+++ naloge/2016/dn2/EvaZmazek/maxindepset.py
@@ -164,36 +164,3 @@ def maxCycleTreeIndependentSet(T, w):
                 uporabljeneTocke += [(i, vozlisceDrevesa)]
 
     return (vrednost, uporabljeneTocke)
-
-"""
-Od tu naprej sta dva primera:
-1. primer: pot
-2. primer: primer iz navodil naloge. Rešitev se razlikuje v enem vozlišču, ki ga jaz ne vzamem,
-            ker je utež na njem enaka 0, in je zato vseeno, če ga vzamemo ali ne. V testni rešitvi
-            v navodilih, ga vzamemo!!!!!!
-"""
-T = [[1], [0, 2], [1]]
-w = [[1, 1, 1],
-     [2, 2, 2],
-     [3, 3, 3],
-     [4, 4, 4]]
-rezultat = maxCycleTreeIndependentSet(T,w)
-print(rezultat)
-vsota = 0
-for i, j in rezultat[1]:
-    vsota += w[i][j]
-print(vsota)
-
-T = [[1, 2], [0, 3, 4], [0, 5], [1, 6, 7], [1, 8], [2, 9, 10], [3], [3], [4, 11], [5], [5, 12], [8], [10, 13], [12]]
-w = [[6, 7, 3, 6, 8, 7, 5, 4, 5, 8, 7, 6, 2, 5],
-      [3, 6, 2, 5, 8, 5, 9, 1, 5, 8, 3, 7, 3, 3],
-      [8, 3, 2, 5, 7, 9, 4, 3, 7, 8, 0, 9, 3, 8],
-      [5, 7, 3, 7, 2, 9, 4, 2, 6, 0, 9, 1, 5, 0]]
-
-rezultat = maxCycleTreeIndependentSet(T, w)
-print(rezultat)
-vsota = 0
-rezultatJ = [(0, 0), (2, 0), (1, 1), (3, 1), (0, 3), (2, 3), (0, 4), (2, 4), (1, 5), (3, 5), (1, 6), (3, 6), (1, 7), (3, 7), (1, 8), (3, 8), (0, 9), (2, 9), (0, 10), (2, 10), (0, 11), (2, 11), (1, 12), (3, 12), (0, 13), (2, 13)]
-for i, j in rezultat[1]:
-    vsota += w[i][j]
-print(vsota)
