191817a4fa70d107c36b534e5187398bcd6fb555
LukaAvbreht <l.avbreht@gmail.com>
2017-02-07 18:01:40 +0100

    Function of checking if child is "proper" writen in o(n)

--- naloge/2016/dn2/LukaAvbreht/otherFunctions.py
+++ naloge/2016/dn2/LukaAvbreht/otherFunctions.py
@@ -1,9 +1,13 @@
 
 
 def primeren_otrok(parent,child):
-    """It returns true if the ciycle child can ba a neighbour cycle of parent (input is in binary)"""
-
-
+    """It returns true if the ciycle child can ba a neighbour cycle of parent (input is in string in binary)"""
+    k = len(parent)
+    assert k != len(child), \
+        "Parent and child must be of same size"
+    for i in range(k):
+        if parent[i] != child[i]:
+            return False
     return True
 
 def list_primernih_otrok(pranet):
