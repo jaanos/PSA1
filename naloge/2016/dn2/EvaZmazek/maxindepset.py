6df6e4b00d6be5e77e70086ee1712e7f956ab9cb
EvaZmazek <eva.zmazek@student.fmf.uni-lj.si>
2017-02-07 19:12:38 +0100

    vsota utezi po teh vozliscih je prava. Je mozno, da resitev v tem primeru ni enolicna?

--- naloge/2016/dn2/EvaZmazek/maxindepset.py
+++ naloge/2016/dn2/EvaZmazek/maxindepset.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def maxCycleTreeIndependentSet(T, w):
-    Tp = [[1], [0, 2], [1]]
-    wp = [[1, 1, 1],
-             [2, 2, 2],
-             [3, 3, 3],
-             [4, 4, 4]]
-
-    T = [[1, 2], [0, 3, 4], [0, 5], [1, 6, 7], [1, 8], [2, 9, 10], [3], [3], [4, 11], [5], [5, 12], [8], [10, 13], [12]]
-    w = [[6, 7, 3, 6, 8, 7, 5, 4, 5, 8, 7, 6, 2, 5],[3, 6, 2, 5, 8, 5, 9, 1, 5, 8, 3, 7, 3, 3],[8, 3, 2, 5, 7, 9, 4, 3, 7, 8, 0, 9, 3, 8],[5, 7, 3, 7, 2, 9, 4, 2, 6, 0, 9, 1, 5, 0]]
     """
     Najtežja neodvisna množica
     v kartezičnem produktu cikla C_k in drevesa T z n vozlišči,
@@ -174,6 +166,33 @@ def maxCycleTreeIndependentSet(T, w):
         print(indexVzorec, vzorcek)
         for i in range(k):
             if vzorcek[i] == 1:
-                uporabljeneTocke += [(k-i-1, vozlisceDrevesa)]
+                uporabljeneTocke += [(i, vozlisceDrevesa)]
 
     return (vrednost, uporabljeneTocke)
+
+
+T = [[1], [0, 2], [1]]
+w = [[1, 1, 1],
+     [2, 2, 2],
+     [3, 3, 3],
+     [4, 4, 4]]
+rezultat = maxCycleTreeIndependentSet(T,w)
+print(rezultat)
+vsota = 0
+for i, j in rezultat[1]:
+    vsota += w[i][j]
+print(vsota)
+
+T = [[1, 2], [0, 3, 4], [0, 5], [1, 6, 7], [1, 8], [2, 9, 10], [3], [3], [4, 11], [5], [5, 12], [8], [10, 13], [12]]
+w = [[6, 7, 3, 6, 8, 7, 5, 4, 5, 8, 7, 6, 2, 5],
+      [3, 6, 2, 5, 8, 5, 9, 1, 5, 8, 3, 7, 3, 3],
+      [8, 3, 2, 5, 7, 9, 4, 3, 7, 8, 0, 9, 3, 8],
+      [5, 7, 3, 7, 2, 9, 4, 2, 6, 0, 9, 1, 5, 0]]
+
+rezultat = maxCycleTreeIndependentSet(T, w)
+print(rezultat)
+vsota = 0
+rezultatJ = [(0, 0), (2, 0), (1, 1), (3, 1), (0, 3), (2, 3), (0, 4), (2, 4), (1, 5), (3, 5), (1, 6), (3, 6), (1, 7), (3, 7), (1, 8), (3, 8), (0, 9), (2, 9), (0, 10), (2, 10), (0, 11), (2, 11), (1, 12), (3, 12), (0, 13), (2, 13)]
+for i, j in rezultat[1]:
+    vsota += w[i][j]
+print(vsota)
