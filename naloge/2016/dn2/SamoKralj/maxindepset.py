727028ff414868ef251f38b878222bafb2b61b0b
Samo Kralj <samokralj95@gmail.com>
2017-02-08 17:47:30 +0100

    Pravilno delujoč algoritem, ki deluje z bitmaski na ciklu in nato poisce najboljso resitev z dinamicnim programiranjem po drevesu navzdol.

--- naloge/2016/dn2/SamoKralj/maxindepset.py
+++ naloge/2016/dn2/SamoKralj/maxindepset.py
@@ -158,3 +158,121 @@ def uredi_po_plasteh(T):
     vrstni_red.sort()
     vrstni_red = [i for k,i in vrstni_red]
     return vrstni_red
+
+
+def maxCycleTreeIndependentSet2(T, w):
+    """
+    Najtežja neodvisna množica
+    v kartezičnem produktu cikla C_k in drevesa T z n vozlišči,
+    kjer ima tabela tež w dimenzije k×n (k >= 2).
+
+    Vrne par (c, s), kjer je c teža najdene neodvisne množice,
+    s pa je seznam vozlišč v neodvisni množici,
+    torej seznam parov oblike (i, u) (0 <= i <= k-1, 0 <= u <= n-1).
+    """
+    n = len(T)
+    assert all(len(r) == n for r in w), \
+        "Dimenzije tabele tež ne ustrezajo številu vozlišč v drevesu!"
+    assert all(all(u in T[v] for v in a) for u, a in enumerate(T)), \
+        "Podani graf ni neusmerjen!"
+    k = len(w)
+    assert k >= 2, "k mora biti vsaj 2!"
+    if n == 0:
+        return (0, [])
+
+    memo = dict()
+
+    mask = generiraj_bitmask(k)
+
+    vrstni_red = uredi_po_plasteh(T)
+
+    print(vrstni_red)
+    print(mask)
+
+    def find_max(koren, indeks_vozlisca):
+        if (koren, indeks_vozlisca) in memo:
+            return memo[koren, indeks_vozlisca]
+        else:
+            maksimum = 0
+            v = vrstni_red[indeks_vozlisca]
+            for vzorec in mask[koren]:
+                teza = vrednost(vzorec, w, k, v)
+                for i in range(indeks_vozlisca + 1, len(vrstni_red)):
+                    if vrstni_red[i] in T[v]:
+                        teza += find_max(vzorec, i)
+                if teza > maksimum:
+                    maksimum = teza
+            memo[koren, indeks_vozlisca] = maksimum
+            return maksimum
+    return find_max(0,0)
+
+def generiraj_bitmask(k):
+    """
+    Vrne vse bitmaske dolžine k, za katera velja, da nobeni dve enici
+    nista sosednji.
+    """
+    assert k >= 2, 'Cikel mora biti dolžine vsaj 2.'
+    mask = []
+    for i in range(2**k):
+        zapis = binarno(i, k)
+        zapis = zapis
+        flag = True
+        for j in range(len(zapis) - 1):
+            if zapis[j] == 1 and zapis[j+1] == 1:
+                flag = False
+                break
+        if zapis[0] == 1 and zapis[-1] == 1:
+            flag = False
+        if flag:
+            mask.append(i)
+
+    povezave = {i : [k for k in mask if (i & k) == 0] for i in mask}
+    return povezave
+
+def binarno(n, k):
+    """
+    Vrne binaren zapis na k mest.
+    """
+    assert n < 2**k, 'Število je preveliko'
+    vrni = []
+    while n > 0:
+        vrni.append(n%2)
+        n = n//2
+    return [0]*(k - len(vrni)) + list(reversed(vrni))
+
+def vrednost(mask, w, k, vozlisce):
+    zapis = binarno(mask, k)
+    teza = 0
+    for i in range(k):
+        teza += zapis[i]*w[i][vozlisce]
+    return teza
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
