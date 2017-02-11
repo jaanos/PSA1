5dfdb34db429a21a70a540e42af5300b121c4483
Žiga Zupančič <zigazupancic5@gmail.com>
2017-02-11 15:46:44 +0100

    Dodaj funkciji za dolocitev podmnozic in kompatibilne podmnozice

--- naloge/2016/dn2/ZigaZupancic/maxindepset.py
+++ naloge/2016/dn2/ZigaZupancic/maxindepset.py
@@ -19,4 +19,41 @@ def maxCycleTreeIndependentSet(T, w):
     assert k >= 2, "k mora biti vsaj 2!"
     if n == 0:
         return (0, [])
-    raise NotImplementedError("Naredi sam!")
+
+    def getSubsets(k):
+        """ Vrne vse možne podmnožice k elementov, predstavljene v binarnem zapisu, kjer '1' na j-tem mestu predstavlja
+        da je element na j-tem mestu v množici. Izloči množice, kjer sta dve '1'-ki sosednji ali na prvem in zadnjem
+        mestu. """
+        subsets = []
+        for i in range(2**k):
+            binary = bin(i)[2:]
+            if len(binary) < k:
+                binary = '0' * (k - len(binary)) + binary
+            first = binary[0]
+            last = binary[-1]
+            if first == '1' and last == '1':
+                continue
+            prev = first
+            subset_ok = True
+            for b in binary[1:]:
+                if prev == '1' and prev == b:
+                    subset_ok = False
+                    break
+                else:
+                    prev = b
+            if subset_ok:
+                subsets.append(i)
+        return subsets
+
+    def getCompatibileSubsets(subsets):
+        """ Vrne slovar kompatibilnih podmnožic za vsako izmed podmnožic. """
+        compatible = dict()
+        for subset in subsets:
+            compatible[subset] = []
+            for s in subsets:
+                if subset & s == 0:
+                    compatible[subset].append(s)
+        return compatible
+
+    subsets = getSubsets(k)
+    compatible = getCompatibileSubsets(subsets)
