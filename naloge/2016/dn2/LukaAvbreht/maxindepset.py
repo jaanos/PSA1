d008ae546f00b80d7cd635b510701a26a83e66d7
LukaAvbreht <l.avbreht@gmail.com>
2017-02-15 16:05:22 +0100

    buty addons

--- naloge/2016/dn2/LukaAvbreht/maxindepset.py
+++ naloge/2016/dn2/LukaAvbreht/maxindepset.py
@@ -20,7 +20,7 @@ def maxCycleTreeIndependentSet(T, w):
         "Dimenzije tabele tež ne ustrezajo številu vozlišč v drevesu!"
     assert all(all(u in T[v] for v in a) for u, a in enumerate(T)), \
         "Podani graf ni neusmerjen!"
-    k = len(w)   #k is the len of the cycle
+    k = len(w)
     assert k >= 2, "k mora biti vsaj 2!"
     if n == 0:
         return (0, [])
@@ -37,37 +37,38 @@ def maxCycleTreeIndependentSet(T, w):
             if i == '1':
                 res+= w[j][nivo]
                 vozlisca_grafa += ((j, nivo),)
-        return (res, vozlisca_grafa)
+        return res, vozlisca_grafa
 
     vsi_mozni_cikli = all_valid_cycles(k)
 
     memo = dict()
 
-    def recursive_max_subgroup(ind_voz,cycle):
-        """Rekurzivna fukncija ki prejme koren v drevesu (n) in njegov vzorec(cycle),
-         ter vrne najvecjo utezeno mnozico na drevesu z korenom v n"""
+    def recursive_max_subgroup(ind_voz, cycle):
+        """
+        Rekurzivna fukncija ki prejme koren v drevesu (n) in njegov vzorec(cycle),
+        ter vrne najvecjo utezeno mnozico na drevesu z korenom v n
+        """
+        # We returnn the value if we calculatet it before and saved it in memo
+        if (ind_voz, cycle) in memo:
+            return memo[(ind_voz, cycle)]
 
-        #We returnn the value if we calculatet it before and saved it in memo
-        if (ind_voz,cycle) in memo:
-            return memo[(ind_voz,cycle)]
-
-        #Computation of largest subgroup
+        # Computation of largest subgroup
         res = 0
         nodes = list()
 
         for patern in vsi_mozni_cikli[cycle]:
-            weight, index = vrednost_cikla_na_nivoju(ind_voz,patern)
+            weight, index = vrednost_cikla_na_nivoju(ind_voz, patern)
             index = list(index)
             for voz in T[ind_voz]:
-                tr_weight, tr_index = recursive_max_subgroup(voz,patern)
+                tr_weight, tr_index = recursive_max_subgroup(voz, patern)
                 weight += tr_weight
                 index += list(tr_index)
             if weight > res:
                 res = weight
                 nodes = index
-        memo[(ind_voz,cycle)] = (res,frozenset(nodes))
-        return (res,frozenset(nodes))
+        memo[(ind_voz, cycle)] = (res, frozenset(nodes))
+        return res, frozenset(nodes)
     cycle = '0'*k
-    res  = recursive_max_subgroup(0,cycle)
+    res = recursive_max_subgroup(0, cycle)
     graf = list(res[1])
-    return (res[0],graf)
+    return res[0], graf
