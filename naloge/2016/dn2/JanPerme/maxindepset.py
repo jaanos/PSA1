7749d49abb982ad2d029b0254d8f3e52655ad923
Jan <jan.perme@gmail.com>
2017-02-17 18:51:04 +0100

    dodano komentarje v kodo

--- naloge/2016/dn2/JanPerme/maxindepset.py
+++ naloge/2016/dn2/JanPerme/maxindepset.py
@@ -22,27 +22,27 @@ def maxCycleTreeIndependentSet(T, w):
     if n == 0:
         return (0, [])
     def drevo(T,w,V,vozlisce,obiskani):
-        obiskani1=obiskani.copy()
+        obiskani1=obiskani.copy() #zapomnemo si vozlisča ki smo jih že obiskali
         obiskani1.append(vozlisce)
         mozni_sosedi=[]
-        for sosed in T[vozlisce]:
+        for sosed in T[vozlisce]:#preverimo v katere sosede lahko gremo
             if sosed not in obiskani1:
                 mozni_sosedi.append(sosed)
         cikel=[i for i in range(k)]
-        for v in V:
+        for v in V:#izločimo vozlišča ki so v knofliktu s množico V izbrano v prejšnem vozlišču
             del cikel[cikel.index(v)]
-        H=memorizacija2(cikel)
-        maxmnozica, maximum = max(enumerate(sum(w[element][vozlisce] for element in M)+sum(memorizacija1(M,sosed,obiskani1)[0] for sosed in mozni_sosedi) for M in H), key=operator.itemgetter(1))
+        H=memorizacija2(cikel)#naredimo vse neodvisne množice
+        maxmnozica, maximum = max(enumerate(sum(w[element][vozlisce] for element in M)+sum(memorizacija1(M,sosed,obiskani1)[0] for sosed in mozni_sosedi) for M in H), key=operator.itemgetter(1)) #izračunamo max vsote trenutne množice + sosedov
         izbrani=mnozica(H[maxmnozica],vozlisce)
-        for sosed in mozni_sosedi:
+        for sosed in mozni_sosedi: #zapomnemo si vozlišča ki smo jih izbrali prej
             izbrani+=memorizacija1(H[maxmnozica],sosed,obiskani1)[1]
         return [maximum,izbrani]
     def neodvisna_mn(A):
         if len(A)!=0:
             mnozica=[[A[0]]]
-            for j in range(1,len(A)):
+            for j in range(1,len(A)):#izbremo element in ga dodamo vsem kompatibilnim množicam
                 pmn=[]
-                for M in mnozica:
+                for M in mnozica: #dodamo element kompatibilnim množicam
                     if A[j]==0 and (1 in M or k-1 in M): pass
                     elif A[j]==(k-1) and (0 in M or k-2 in M):pass
                     elif (A[j]+1) in M or (A[j]-1) in M: pass
@@ -50,16 +50,16 @@ def maxCycleTreeIndependentSet(T, w):
                         element=M.copy()
                         pmn+=[element+[A[j]]]
                 mnozica+=pmn
-            mnozica+=memorizacija2(A[1:])
+            mnozica+=memorizacija2(A[1:])#rekurzivno dodamo še preostale množice ki ne vsebujejo A[0]
             return mnozica
         else:
             return [[]]
-    def mnozica(G,vozlisce):
+    def mnozica(G,vozlisce):#iz mnozice in vozlišča naredimo pare
         A=[]
         for a in G:
             A.append((a,vozlisce))
         return A
-    def memorizacija1(V,sosed,obiskani1):
+    def memorizacija1(V,sosed,obiskani1): #memorizacija za funkcijo drevo
         A=frozenset(V)
         B=frozenset(obiskani1)
         index=(A,sosed,B)
@@ -68,7 +68,7 @@ def maxCycleTreeIndependentSet(T, w):
         else:
             slovar[index]=drevo(T,w,V,sosed,obiskani1)
             return slovar[index]
-    def memorizacija2(A):
+    def memorizacija2(A): #memorizacija za funkcijo neodvisna_mn
         B=frozenset(A)
         if B in slovar2:
             return slovar2[B]
